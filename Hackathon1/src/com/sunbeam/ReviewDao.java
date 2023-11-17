4127238010ad9106b069a638dcfc7433a8fe1e40
D4_80737_SagarKhatri <sagarkhatri.pro@gmail.com>
2023-11-17 15:04:51 +0530

    Main Functions implemented

--- Hackathon1/src/com/sunbeam/ReviewDao.java
+++ Hackathon1/src/com/sunbeam/ReviewDao.java
@@ -1,5 +1,51 @@
 package com.sunbeam;
 
-public class ReviewDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+public class ReviewDao implements AutoCloseable{
+    private Connection con;
+    private PreparedStatement stmtCreateReview;
+    private PreparedStatement stmtEditReview;
+    private PreparedStatement stmtDeleteReview;
+    private PreparedStatement stmtDisplayAllReview;
+    private PreparedStatement stmtDisplayMyReviews;
+
+    public ReviewDao() throws Exception{
+        con = DBUtil.getCon();
+
+        stmtCreateReview = con.
+                prepareStatement("Insert into reviews values(default, ?,?,?,?,?)");
+        stmtEditReview = con.prepareStatement("Update reviews set review = ? rating = ? modified = now()");
+        stmtDeleteReview = con.prepareStatement("Delete reviews where id = ?");
+        stmtDisplayAllReview = con.prepareStatement("Select * from reviews");
+        stmtDisplayMyReviews = con.prepareStatement("select * from reviews where user_ id = ?");
+    }
+
+    public int createReview(ReviewsPOJO r) throws Exception{
+        stmtCreateReview.setInt(1, r.getMovie_id());
+        stmtCreateReview.setString(2, r.getReview());
+        stmtCreateReview.setInt(3, r.getRating());
+        stmtCreateReview.setInt(4, r.getUser_id());
+        stmtCreateReview.setTimestamp(5, r.getModified());
+        return stmtCreateReview.executeUpdate();
+    }
+
+    public void editReview(){
+
+    }
+
+
+    public void deleteReview(){}
+
+
+    public void displayAllReviews(){}
+
+
+    public void displayMyReviews(){}
+
+    @Override
+    public void close() throws Exception {
+        con.close();
+    }
 }
