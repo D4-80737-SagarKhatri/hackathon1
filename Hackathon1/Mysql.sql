e730e6720ee8c8c60af29e4307a3ba023f63e904
D4_80737_SagarKhatri <sagarkhatri.pro@gmail.com>
2023-11-17 11:48:07 +0530

    sql modified

--- Hackathon1/Mysql.sql
+++ Hackathon1/Mysql.sql
@@ -1,3 +1,16 @@
-create table users (id int primary key,first_name varchar(40),last_name varchar(40),email varchar(40),password varchar(40),mobile varchar(10),birth Date);
+create table users (id int primary key,
+first_name varchar(40),
+last_name varchar(40),
+email varchar(40),
+password varchar(40),
+mobile varchar(10),
+birth Date);
 
-create table reviews(id int primary key, movie_id int ,review varchar(40),rating int,user_id int,modified timestamp, foregin key (user_id) references users(id));
+create table reviews(
+id int primary key,
+movie_id int,
+review varchar(40),
+rating int,
+user_id int,
+modified timestamp,
+foreign key (user_id) references users(id));
