9ead3f6da0ff0937fb9a6da692549a4317af20e5
Sneharakshe <ayushrakshe2916@gmail.com>
2023-11-17 11:43:33 +0530

    a

---
+++ Hackathon1/Mysql.sql
@@ -0,0 +1,3 @@
+create table users (id int primary key,first_name varchar(40),last_name varchar(40),email varchar(40),password varchar(40),mobile varchar(10),birth Date);
+
+create table reviews(id int primary key, movie_id int ,review varchar(40),rating int,user_id int,modified timestamp, foregin key (user_id) references users(id));
