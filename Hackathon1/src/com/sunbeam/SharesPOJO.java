4622a5a9be3b946713f490c0c30a167979c0ff8f
Sneharakshe <ayushrakshe2916@gmail.com>
2023-11-17 12:15:09 +0530

    a

---
+++ Hackathon1/src/com/sunbeam/SharesPOJO.java
@@ -0,0 +1,11 @@
+package com.sunbeam;
+
+public class SharesPOJO {
+    private int reviewId;
+    private int userId;
+
+    public SharesPOJO(int reviewId, int userId) {
+        this.reviewId = reviewId;
+        this.userId = userId;
+    }
+}
